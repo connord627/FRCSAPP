11f9ba2792bc497c86b3763cc3c322cecfc0bd86
connord627 <72620674+connord627@users.noreply.github.com>
2023-08-15 18:09:08 -0400

    Create User.js

---
+++ User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MyBooks = () => {
+  return (
+    <View>
+      <Text>Your Books go here!</Text>
+    </View>
+  );
+};
+
+export default MyBooks;
