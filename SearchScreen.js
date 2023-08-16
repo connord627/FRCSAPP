5fbfbfca990b2fb9352ed3be2da4c44be93bb5e7
connord627 <connor.davidson@tufts.edu>
2023-08-16 18:40:39 -0400

    Update SearchScreen.js

--- SearchScreen.js
+++ SearchScreen.js
@@ -1,49 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const SearchScreen = () => {
+  const [searchText, setSearchText] = useState(''); 
+
+  const handleSearchTextChange = (text) => {
+    setSearchText(text); 
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the FRCS library!</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => console.log('Button Pressed')}>
-        <Text style={styles.buttonText}>Search Books</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => console.log('Button Pressed')}>
-        <Text style={styles.buttonText}>My Books</Text>
-      </TouchableOpacity>
+    <View style = {styles.container}>
+      <Text>Search Screen</Text>
+      <TextInput
+        onChangeText={handleSearchTextChange}
+        value={searchText}
+        style={styles.inputBox}
+        placeholder='Search'
+        placeholderTextColor='gray'
+        backgroundColor='white'
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  inputBox: {
+    padding: 7,
+    borderColor: 'gray',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    marginRight: 0,
+    marginTop: 0,
+    borderRadius: 5,
+    color: 'black',
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 30,
-  },
-  button: {
-    backgroundColor: '#3c329c',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginBottom: 15,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
+    justifyContent: 'flex-start',
+    paddingTop: 5,
+    backgroundColor: 'white',
+  }
 });
 
 export default SearchScreen;
