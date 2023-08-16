4f71f5e58215b9a218de18281df6c393eaf8ccfa
cassied1028 <cassied1028@gmail.com>
2023-08-16 19:18:15 -0400

    Update User.js

--- User.js
+++ User.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 const MyBooks = () => {
   return (
-    <View>
-      <Text>Your Books go There!</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Text style={styles.username} adjustsFontSizeToFit numberOfLines={1}>
+          @username
+        </Text>
+        <Text style={styles.subtitle}>{}0 books borrowed</Text>
+        <View style = {styles.horizontalLine}></View>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  topView: {
+    backgroundColor: 'white',
+  },
+
+  username: {
+    fontSize: 35,
+    textAlign: 'left',
+    margin: 10,
+
+  },
+  subtitle: {
+    fontSize: 20,
+    textAlign: 'left',
+    marginLeft: 10,
+  },
+
+  horizontalLine: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginVertical: 10,
+  },
+
+});
+
 
 export default MyBooks;
